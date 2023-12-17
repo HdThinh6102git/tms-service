984df2c142c10b599fef47c817a8d512c2dc0edf
HdThinh6102git <lck0601111202@gmail.com>
2023-12-17 16:05:45 +0700

    fix: api get students project - seperate role api

--- src/user/controllers/student-project/student-project.controller.ts
+++ src/user/controllers/student-project/student-project.controller.ts
@@ -1,13 +1,15 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
 import { BasePaginationResponse } from '../../../shared/dtos';
 import { StudentProjectService } from '../../providers';
 import { StudentProjectFilter, StudentProjectOutput } from '../../dtos';
+import { JwtAuthGuard } from '../../../auth/guards';
 
 @Controller('student-project')
 export class StudentProjectController {
   constructor(private studentProjectService: StudentProjectService) {}
 
   @Get('/filter')
+  @UseGuards(JwtAuthGuard)
   public async getStudentProjects(
     @Query() query: StudentProjectFilter,
   ): Promise<BasePaginationResponse<StudentProjectOutput>> {
