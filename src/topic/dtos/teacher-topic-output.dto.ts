47b880bb852ee9d24e630e33e5bea4fe73c122da
HdThinh6102git <lck0601111202@gmail.com>
2023-12-22 09:25:37 +0700

    fix: change teacher topic output file format

--- src/topic/dtos/teacher-topic-output.dto.ts
+++ src/topic/dtos/teacher-topic-output.dto.ts
@@ -1,61 +1,8 @@
-import { Expose, Type } from 'class-transformer';
+import { Expose } from 'class-transformer';
 import { ApiProperty } from '@nestjs/swagger';
-import { AdminOutput, UserOutputDto } from '../../user/dtos';
-import { MajorOutput } from '../../major/dtos';
+import { TopicOutput } from './topic-output.dto';
 
-export class TeacherTopicOutput {
-  @Expose()
-  @ApiProperty()
-  public id: string;
-
-  @Expose()
-  @ApiProperty()
-  public name: string;
-
-  @Expose()
-  @ApiProperty()
-  public detail: string;
-
-  @Expose()
-  @ApiProperty()
-  @Type(() => Number)
-  public status: number;
-
-  @Expose()
-  @ApiProperty()
-  @Type(() => Date)
-  public startDate: Date;
-
-  @Expose()
-  @ApiProperty()
-  @Type(() => Date)
-  public finishDate: Date;
-
-  @Expose()
-  @ApiProperty()
-  @Type(() => UserOutputDto)
-  public reviewTeacher: UserOutputDto | null;
-
-  @Expose()
-  @ApiProperty()
-  @Type(() => Date)
-  public createdAt: Date;
-
-  @Expose()
-  @ApiProperty()
-  @Type(() => Date)
-  public updatedAt: Date;
-
-  @Expose()
-  @ApiProperty()
-  @Type(() => AdminOutput)
-  admin: AdminOutput;
-
-  @Expose()
-  @ApiProperty()
-  @Type(() => MajorOutput)
-  major: MajorOutput;
-
+export class TeacherTopicOutput extends TopicOutput {
   @Expose()
   @ApiProperty()
   public isYourRegistration: boolean;
